4b1d4f9908c87af8d1a0eb316af4d39b37363f49
danny galimi <dannygalimi13@aim.com>
2021-02-19 19:44:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,9 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    self.name = student_hash[:name]
+    self.location = student_hash[:location]
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
@@ -17,7 +19,7 @@ class Student
   end
 
   def self.all
-    
+    @@all
   end
 end
 
