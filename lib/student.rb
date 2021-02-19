a1fb7e1d6bcd9b9c1a652aad061db1dc197a5127
danny galimi <dannygalimi13@aim.com>
2021-02-19 20:46:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,7 @@ class Student
 
   def add_student_attributes(attributes_hash)
     attributes_hash.each do |info,value|
-      self.send("#{info}",value)
+      self.send("#{info}=",value)
       self
     end 
     
