f5f2506e273c29f43ec80e9ba4d8cb39ec42b6ae
danny galimi <dannygalimi13@aim.com>
2021-02-19 20:18:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,8 +11,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-   
-    students_array.each do |student|
+      students_array.each do |student|
       student_new = Student.new(student)
       end
   end
